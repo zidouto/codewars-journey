bf1185d0a894b75d1886998432b35bd642c14b18
zidouto <czeslame@gmail.com>
2024-09-06 00:04:37 +0700

    Kyu 7 shortest word

---
+++ 7Kyu/shortestWord.js
@@ -0,0 +1,18 @@
+/*
+Simple, given a string of words, return the length of the shortest word(s).
+
+String will never be empty and you do not need to account for different data types.
+*/
+
+function findShort(s){
+    let arr = s.split(' ')
+    let result = []
+    for (let i = 0 ; i < arr.length ; i++){
+        result.push(arr[i].length)
+    }
+    return Math.min(...result)
+}
+
+console.log(findShort('bitcoin take over the world maybe who knows perhaps'))
+console.log(findShort('turns out random test cases are easier than writing out basic ones'))
+console.log(findShort("Let's travel abroad shall we"))
