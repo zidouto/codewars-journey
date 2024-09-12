3930ca78af68b011a076657faa898cc7b6f21259
zidouto <czeslame@gmail.com>
2024-09-13 00:12:39 +0700

    Kyu 7

---
+++ 7Kyu/friendorFoe.js
@@ -0,0 +1,23 @@
+/*
+Make a program that filters a list of strings and returns a list with only your friends name in it.
+If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not...
+Example:
+ Input = {"Ryan", "Kieran", "Jason", "Yous"}
+ Output = {"Ryan", "Yous"}
+
+ Input = {"Peter", "Stephen", "Joe"}
+ Output = {}
+Input strings will only contain letters. Note: keep the original order of the names in the output.
+*/
+
+function friend(friends){
+    let myFriends = []
+    for (let i = 0 ; i < friends.length ; i++){
+        if (friends[i].length === 4){
+            myFriends.push(friends[i])
+        }
+    }
+    return myFriends
+}
+
+console.log(friend(["Ryan", "Kieran", "Jason", "Yous"]))
