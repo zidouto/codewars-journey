aedc65dd2ae189afe792746e03b787bcc9321245
zidouto <czeslame@gmail.com>
2024-09-16 23:25:49 +0700

    Kyu 8 Lol

---
+++ 8Kyu/combineStrings.js
@@ -0,0 +1,12 @@
+/*
+Create a function named combineNames/combine_names/CombineNames that accepts two parameters (first and last name).
+The function should return the full name.
+
+Example:
+
+With "James" as the first name and "Stevens" as the last name should return "James Stevens"
+*/
+
+function combineNames(name1, name2){
+    return `${name1} ${name2}`
+  }
