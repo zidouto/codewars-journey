f745613caa490268100d80e5b178294b6aa08468
zidouto <czeslame@gmail.com>
2024-09-16 23:30:19 +0700

    Add  1 test case

--- 8Kyu/combineStrings.js
+++ 8Kyu/combineStrings.js
@@ -3,10 +3,11 @@ Create a function named combineNames/combine_names/CombineNames that accepts two
 The function should return the full name.
 
 Example:
-
 With "James" as the first name and "Stevens" as the last name should return "James Stevens"
 */
 
 function combineNames(name1, name2){
     return `${name1} ${name2}`
-  }
+}
+
+console.log(combineNames('James', 'Steven'))
