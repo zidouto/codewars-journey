f48830cbc9bb57ff835ca60788fb397e82018789
zidouto <czeslame@gmail.com>
2024-09-18 00:10:36 +0700

    Revise comment

--- 7Kyu/counter.js
+++ 7Kyu/counter.js
@@ -10,7 +10,7 @@ newCounter() // 2
 
 Closure:
 A closure is the combination of a function bundled together (enclosed) with references to its surrounding state (the lexical environment). 
-In other words, a closure gives you access to an outer function’s scope from an inner function. 
+In other words, a closure gives you access to an outer function's scope from an inner function. 
 In JavaScript, closures are created every time a function is created, at function creation time.
 */
 
