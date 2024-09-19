6dd2c344b4a73cb34899faeb7a7b4bafa7119e0c
zidouto <czeslame@gmail.com>
2024-09-19 22:21:48 +0700

    Delete 1 Test case

--- 7Kyu/categorizeNewMember.js
+++ 7Kyu/categorizeNewMember.js
@@ -28,4 +28,3 @@ function openOrSenior(data){
 
 console.log(openOrSenior([[45, 12],[55,21],[19, -2],[104, 20]]))
 console.log(openOrSenior([[3, 12],[55,1],[91, -2],[53, 23]]))
-console.log(openOrSenior([[59, 12],[55,-1],[12, -2],[12, 12]]))
