39854e70293c3009c36c9d93188dce64c3c4dba8
zidouto <czeslame@gmail.com>
2024-09-22 22:54:53 +0700

    Another delete test case on shortestWord Kyu7

--- 7Kyu/shortestWord.js
+++ 7Kyu/shortestWord.js
@@ -15,4 +15,3 @@ function findShort(s){
 
 console.log(findShort('bitcoin take over the world maybe who knows perhaps'))
 console.log(findShort('turns out random test cases are easier than writing out basic ones'))
-console.log(findShort("Let's travel abroad shall we"))
