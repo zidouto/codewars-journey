cae8da6568ccaff3eab42242269a667937a4608b
zidouto <czeslame@gmail.com>
2024-09-26 22:24:00 +0700

    Delete 1 test case on Kyu 7 growthOfPopulation.js

--- 7Kyu/growthOfPopulation.js
+++ 7Kyu/growthOfPopulation.js
@@ -38,4 +38,3 @@ function nbYear(p0, percent, aug, p, totalYears = 0) {
 console.log(nbYear(1500, 5, 100, 5000)) //15
 console.log(nbYear(1500000, 2.5, 10000, 2000000)) //10
 console.log(nbYear(1500000, 0.25, 1000, 2000000)) //94
-console.log(nbYear(1000, 2.0, 50, 1214)) //4
