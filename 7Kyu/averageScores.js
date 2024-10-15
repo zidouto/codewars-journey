a07cd608bf6ec4f89d64993c83dbfa25b6c1868f
zidouto <czeslame@gmail.com>
2024-10-15 22:16:53 +0700

    Kyu 7 - Average Scores

---
+++ 7Kyu/averageScores.js
@@ -0,0 +1,8 @@
+function average(scores) {
+    const sum = scores.reduce((acc, score) => acc + score, 0);
+    return Math.round(sum / scores.length);
+}
+
+console.log(average([2,5,72,6,2,6]))
+console.log(average([90,98,89,100,100,86,94]))
+console.log(average([90,98,89,100,100,86,94]))
